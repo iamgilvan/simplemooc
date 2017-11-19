cd0bdeaa8fdfcb4e25a4221f95982794cf1faf09
Gilvan Almeida <g6almeida@hotmail.com>
2017-11-18 22:23:48 -0200

    App Courses

---
+++ simplemooc/courses/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Course(models.Model):
+	
+	name        = models.CharField('Name', max_length = 100)
+	slug        = models.SlugField('Trigger')
+	description = models.TextField('Description', blank=True)
+	start_date  = models.DateField('Start Date', null=True, blank=True)
+	image       = models.ImageField(upload_to='courses/image', verbose_name='Image')
+
+	create_at   = models.DateTimeField('Create in ', auto_now_add=True)
+	update_at   = models.DateTimeField('Update in ', auto_now_add=True)
