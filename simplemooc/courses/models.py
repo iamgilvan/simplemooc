480a69235f977346a17aae513ea802d588440d95
Gilvan Almeida <g6almeida@hotmail.com>
2017-11-18 22:43:43 -0200

    [Alteção] Campo 'image'

--- simplemooc/courses/models.py
+++ simplemooc/courses/models.py
@@ -8,7 +8,7 @@ class Course(models.Model):
 	slug        = models.SlugField('Trigger')
 	description = models.TextField('Description', blank=True)
 	start_date  = models.DateField('Start Date', null=True, blank=True)
-	image       = models.ImageField(upload_to='courses/image', verbose_name='Image')
+	image       = models.ImageField(upload_to='courses/image', verbose_name='Image', null=True, blank=True)
 
 	create_at   = models.DateTimeField('Create in ', auto_now_add=True)
 	update_at   = models.DateTimeField('Update in ', auto_now_add=True)
