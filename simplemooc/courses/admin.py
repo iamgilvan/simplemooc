3762801d0189bd670ad896ab1471b984a702be8d
Gilvan Almeida <g6almeida@hotmail.com>
2017-11-20 00:55:43 -0200

    [Admin] Preenchendo o slug(Trigger) automaticamente.

--- simplemooc/courses/admin.py
+++ simplemooc/courses/admin.py
@@ -4,7 +4,8 @@ from .models import Course
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
 	"""docstring for CourseAdmin"""
-	list_display  = ['name', 'slug', 'start_date', 'update_at']
-	search_fields = ['name', 'slug']	
+	list_display       = ['name', 'slug', 'start_date', 'update_at']
+	search_fields      = ['name', 'slug']	
+	prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Course, CourseAdmin)
