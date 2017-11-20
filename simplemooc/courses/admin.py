2991e05077c219ad6311708e57820d053ba48ddc
Gilvan Almeida <g6almeida@hotmail.com>
2017-11-20 00:38:32 -0200

    [Admin] Model Admin.

--- simplemooc/courses/admin.py
+++ simplemooc/courses/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Course
 
 # Register your models here.
-admin.site.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+	"""docstring for CourseAdmin"""
+	list_display  = ['name', 'slug', 'start_date', 'update_at']
+	search_fields = ['name', 'slug']	
+
+admin.site.register(Course, CourseAdmin)
