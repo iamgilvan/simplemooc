2ef6eb905211f1a8310574e8251cfd2c02d48f39
Gilvan Almeida <g6almeida@hotmail.com>
2017-11-26 00:22:29 -0200

    [Form no Template]  Submetendo o form do curso

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import Course
+from .forms import ContactCourse
 
 def index(request):
     courses = Course.objects.all()
@@ -19,9 +20,18 @@ def index(request):
 #     return render(request, template_name, context)
 
 def details(request, slug):
-    course = get_object_or_404(Course, slug=slug)
-    context = {
-        'course': course
-    }
+    course  = get_object_or_404(Course, slug=slug)
+    context = {}
+    #valid form?
+    if request.method == 'POST':
+        form = ContactCourse(request.POST)
+        if form.is_valid():
+            context['is_valid'] = True
+            print(form.cleaned_data)
+            form = ContactCourse()
+    else:
+        form = ContactCourse()
+    context['course'] = course
+    context['form'] = form
     template_name = 'courses/details.html'
     return render(request, template_name, context)
