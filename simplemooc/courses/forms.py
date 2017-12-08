cecac6f1dca74e39761572ca036171b9ae648be0
Gilvan Almeida <g6almeida@hotmail.com>
2017-12-08 10:43:41 -0200

    [Template] Organizando o envio de e-mail.

--- simplemooc/courses/forms.py
+++ simplemooc/courses/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.core.mail import send_mail
 from django.conf import settings
 
+from simplemooc.core.mail import send_mail_template
+
 class ContactCourse(forms.Form):
     name   = forms.CharField(label='Name', max_length=100)
     email  = forms.EmailField(label='E-mail')
@@ -9,17 +11,19 @@ class ContactCourse(forms.Form):
 
     def send_mail(self, course):
         subject = '[%s] Contato' % course
-        message = 'Nome: %(name)s;E-mail: %(email)s;%(message)s'
+        #message = 'Nome: %(name)s;E-mail: %(email)s;%(message)s'
         context = {
             'name': self.cleaned_data['name'],
             'email': self.cleaned_data['email'],
             'message': self.cleaned_data['message'],
         }
+        #message = message % context
+        template_name = 'courses/contact_email.html'
         mailto = self.cleaned_data['email']
-        message = message % context
-        send_mail(
-                subject,
-                message,
-                settings.DEFAULT_FROM_EMAIL,
-                [mailto]
+        #send_mail(subject,message,settings.DEFAULT_FROM_EMAIL,[mailto])
+        send_mail_template(
+            subject,
+            template_name,
+            context,
+            [mailto]
         )
