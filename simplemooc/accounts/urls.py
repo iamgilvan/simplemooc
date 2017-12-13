68de9f9c200145e35bcc3e7f89eb62fada8fd0f7
Gilvan Almeida <g6almeida@hotmail.com>
2017-12-13 00:56:54 -0200

    [Login] Form de cadastro.

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url , include
 from django.contrib.auth.views import login
-
+from simplemooc.accounts.views import register
 urlpatterns = [
     url(r'^entrar/$', login, {'template_name': 'accounts/login.html'} ,name='login'),
+    url(r'^cadastre-se/$', register, name='register'),
 ]
