148f2a8620e734f02858f92e1e4fccd18c1fd7ac
Gilvan Almeida <g6almeida@hotmail.com>
2017-12-20 21:36:58 -0200

    [Template] Painel do usuário.

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url , include
 from django.contrib.auth.views import login
 from django.contrib.auth.views import logout
 from simplemooc.accounts.views import register
+from simplemooc.accounts.views import dashboard
+
 urlpatterns = [
+    url(r'^$', dashboard ,name='dashboard'),
     url(r'^entrar/$', login, {'template_name': 'accounts/login.html'} ,name='login'),
     url(r'^cadastre-se/$', register, name='register'),
     url(r'^sair/$', logout, {'next_page': 'core:home'} ,name='logout'),
--- simplemooc/accounts/views.py
+++ simplemooc/accounts/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.conf import settings
 
 from .forms import RegisterForm
 
 # Create your views here.
+# Verifica se o usuário esta de fato logado
+@login_required
+def dashboard(request):
+    template_name = 'accounts/dashboard.html'
+    return render(request, template_name)
+
+
 def register(request):
     template_name = 'accounts/register.html'
     if request.method == 'POST':
