1ea40fb730ab6f848ae96c7c8181a9621ec1c492
Gilvan Almeida <g6almeida@hotmail.com>
2017-12-20 01:11:29 -0200

    [Login] View de logout.

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url , include
 from django.contrib.auth.views import login
+from django.contrib.auth.views import logout
 from simplemooc.accounts.views import register
 urlpatterns = [
     url(r'^entrar/$', login, {'template_name': 'accounts/login.html'} ,name='login'),
     url(r'^cadastre-se/$', register, name='register'),
+    url(r'^sair/$', logout, {'next_page': 'core:home'} ,name='logout'),
 ]
