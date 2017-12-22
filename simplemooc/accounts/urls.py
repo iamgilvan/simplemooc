dd02e6f764d56917700737019d415cb8381acf07
Gilvan Almeida <g6almeida@hotmail.com>
2017-12-22 00:39:31 -0200

    [Usuário] Edição de senha.

--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^entrar/$', login, {'template_name': 'accounts/login.html'} ,name='login'),
     url(r'^cadastre-se/$', views.register, name='register'),
     url(r'^editar/$', views.edit, name='edit'),
+    url(r'^editar-senha/$', views.edit_password, name='edit_password'),
     url(r'^sair/$', logout, {'next_page': 'core:home'} ,name='logout'),
 ]
