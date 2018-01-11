6de97e0586a7dde3d5d17e5861106037f6356410
Gilvan Almeida <g6almeida@hotmail.com>
2018-01-11 00:02:02 -0200

    Form para criar nova senha

--- simplemooc/accounts/forms.py
+++ simplemooc/accounts/forms.py
@@ -2,6 +2,11 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 
+from simplemooc.core.utils import generate_hash_key
+from simplemooc.core.mail import send_mail_template
+
+from .models import PasswordReset
+
 User = get_user_model()
 
 #Será um form normal. Não será um ModelForm, pois inicialmente não estará assoociado a nenhum outro model
@@ -14,6 +19,20 @@ class PasswordResetForm(forms.Form):
         if User.objects.filter(email=email).exists():
             return email
         raise forms.ValidationError('Nenhum usuário encontrado com este email.')
+
+    def save(self):
+        #se existe usuário
+        user = User.objects.get(email=self.cleaned_data['email'])
+        #gerar a chave
+        key   = generate_hash_key(user.username)
+        reset = PasswordReset(key=key, user=user)
+        reset.save()
+        template_name = 'accounts/password_reset_email.html'
+        subject = 'Criar nova senha no Simple MOOC'
+        context = {
+            'reset': reset,
+        }
+        send_mail_template(subject, template_name, context, [user.email])
     
 # Esta classe foi cirada para adicionar o campo email
 # Ela extende a classe UserCreationForm e adiciona o email
--- simplemooc/accounts/urls.py
+++ simplemooc/accounts/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     url(r'^entrar/$', login, {'template_name': 'accounts/login.html'} ,name='login'),
     url(r'^cadastre-se/$', views.register, name='register'),
     url(r'^nova-senha/$', views.password_reset, name='password_reset'),
+    url(r'^confirma-nova-senha/(?P<key>\w+)/$', views.password_reset_confirm, name='password_reset_confirm'),
     url(r'^editar/$', views.edit, name='edit'),
     url(r'^editar-senha/$', views.edit_password, name='edit_password'),
     url(r'^sair/$', logout, {'next_page': 'core:home'} ,name='logout'),
