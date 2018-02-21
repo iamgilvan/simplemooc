ca5aa811570c76671ce24fa33837bf7825b01799
Gilvan Praxedes de Almeida <g6almeida@hotmail.com>
2018-02-21 00:50:40 -0300

    Página do anúncio e comntários.

--- simplemooc/courses/views.py
+++ simplemooc/courses/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, get_object_or_404 , redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
-from .models import Course
-from .models import Enrollment
+from .models import Course, Enrollment, Announcement
 from .forms import ContactCourse
 
 def index(request):
@@ -82,5 +81,23 @@ def announcements(request, slug):
     context  = {
         'course': course,
         'announcements': course.announcements.all()
+    }
+    return render(request, tamplate, context)
+
+@login_required
+def show_announcement(request, slug, pk):
+    course = get_object_or_404(Course, slug=slug)
+    #Checar se usuário está inscrito no curso
+    if not request.user.is_staff:
+        enrollment = get_object_or_404(Enrollment,  user= request.user, course=course)
+        if not enrollment.is_approved():
+            messages.error(request, "A sua inscrição está pendente")
+            return redirect('accounts:dashboard')
+    tamplate = 'courses/show_announcement.html'
+    #Obter anuncio do curso relacionado
+    announcement = get_object_or_404(course.announcements.all(), pk=pk)
+    context  = {
+        'course': course,
+        'announcement': announcement
     }
     return render(request, tamplate, context)
