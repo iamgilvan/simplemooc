3362d1c238b707c8bec6b54811d45efd9578a9f7
Gilvan Praxedes de Almeida <g6almeida@hotmail.com>
2018-02-27 00:13:28 -0300

    Setar bd local.

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    #libs
+    'taggit',
+    #apps,
     'simplemooc.core',
     'simplemooc.accounts',
     'simplemooc.courses',
